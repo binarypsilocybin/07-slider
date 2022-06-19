c616ed53287d3b0835f15f5216a79772e9da74c0
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-06-19 17:52:31 +0100

    3 files modified, 2 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
-import "./style.css";
-
-export default function App() {
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+          return (
+            <article key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+      </div>
+    </section>
   );
 }
+
+export default App;
