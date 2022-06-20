d58e712fb65937b9c8e0c2019c154cc00fa3b8d6
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-06-20 08:45:28 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -25,6 +25,12 @@ function App() {
             </article>
           );
         })}
+        <button className="prev">
+          <FiChevronLeft />
+        </button>
+        <button className="next">
+          <FiChevronRight />
+        </button>
       </div>
     </section>
   );
